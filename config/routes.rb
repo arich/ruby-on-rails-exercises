5ca0f53c43d7620586fc01a3ba748486645692fb
arich <andrew.joseph.rich@gmail.com>
2013-10-14 21:00:55 -0500

    Done up to exercise 2.1 besides the last step of exercise 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Bikeshop::Application.routes.draw do
+  resources :orders
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
