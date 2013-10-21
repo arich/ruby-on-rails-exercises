59df8bd2cf7aa4596fddbbac9eafb46ebceadcae
arich <andrew.joseph.rich@gmail.com>
2013-10-20 20:55:10 -0500

    Finished exercise set 2 (with a little help from solutions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
+    @unpaid_order_count = Order.unpaid.count
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ActiveRecord::Base
+	validates_presence_of :customer_name, :customer_email, :description, :price
+
+	scope :unpaid, -> { where("paid_for_on is null") }
 end
